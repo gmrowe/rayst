d494942f708ffb5a77e50999afaa63ee1c721596
Garrett <garrett.m.rowe@gmail.com>
2022-02-08 23:45:32 -0700

    refactored and simplified the code to render ppm

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ mod canvas;
 mod color;
 mod math_helpers;
 mod tup;
+mod matrix;
 
 use tup::Tup;
 use canvas::Canvas;
 use color::Color;
-use std::fs::write;
+use std::fs;
 
 fn plot(point: &Tup, c: Canvas, color: Color) -> Canvas {
     let x = point.x.round() as usize;
@@ -18,8 +19,10 @@ fn plot(point: &Tup, c: Canvas, color: Color) -> Canvas {
     }
 }
 
+//fn write_to_file()
+
 fn main() -> std::io::Result<()> {
-    use std::iter::successors;
+    use std::iter;
     let start = Tup::point(0.0, 1.0, 0.0);
     let velocity = Tup::vector(1.0, 1.8, 0.0).normalize() * 11.25;
     let projectile = Projectile::new(start, velocity);
@@ -30,17 +33,14 @@ fn main() -> std::io::Result<()> {
 
     const WIDTH: usize = 900;
     const HEIGHT: usize = 550;
-    let mut canvas = Canvas::new(WIDTH, HEIGHT);
     let red = Color::new(1.0, 0.0, 0.0);
 
-    let flight = successors(Some(projectile), |p| Some(environment.tick(p)))
-        .take_while(|p| p.pos.y > 0.0);
+    let image = iter::successors(Some(projectile), |p| Some(environment.tick(p)))
+        .take_while(|p| p.pos.y > 0.0)
+        .fold(Canvas::new(WIDTH, HEIGHT), |c, p| plot(&p.pos, c, red))
+        .to_ppm();
 
-    for p in flight {
-        canvas = plot(&p.pos, canvas, red);
-    }
-    let ppm = canvas.to_ppm();
-    write("projectile.ppm", ppm)?;
+    fs::write("projectile.ppm", image)?;
     Ok(())
 }
 
