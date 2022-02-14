4b43d9bcec579e4f37f4527dec66038dbffeda75
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-02-13 23:26:59 -0700

    Update Mat2.determinant() method to use index syntax

--- src/matrix.rs
+++ src/matrix.rs
@@ -237,7 +237,7 @@ impl Mat2 {
         // | a, b |
         // | c, d |
         // ad - bc == determinant
-        self.data[0] * self.data[3] - self.data[1] * self.data[2]
+        self[(0, 0)] * self[(1, 1)] - self[(0, 1)] * self[(1, 0)]
     }
 
 }
