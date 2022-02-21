6cb76d918872f645c4d36ea77645276069291786
Garrett <garrett.m.rowe@gmail.com>
2022-02-21 03:50:58 -0700

    Implement Material::lighting function and associtated tests

--- src/materials.rs
+++ src/materials.rs
@@ -1,4 +1,6 @@
-use crate::Color;
+use crate::color::Color;
+use crate::tup::Tup;
+use crate::lights::Light;
 
 #[derive(PartialEq, Copy, Clone, Debug)]
 pub struct Material {
@@ -35,6 +37,39 @@ impl Material {
 
     pub fn shininess(&self) -> f64 {
         self.shininess
+    }
+
+    pub fn lighting(
+        &self,
+        light: Light,
+        position: Tup,
+        eyev: Tup,
+        normalv: Tup
+    ) -> Color
+    {
+        let black = Color::new(0, 0, 0);
+        let effective_color = self.color() * light.intensity();
+        let lightv = (light.position() - position).normalize();
+        let ambient = effective_color * self.ambient();
+        let light_dot_normal = lightv.dot(&normalv);
+        let diffuse = if light_dot_normal < 0.0 {
+            black
+        } else {
+            effective_color * self.diffuse() * light_dot_normal
+        };
+        let specular = if light_dot_normal < 0.0 {
+            black
+        } else {
+            let reflectv = -lightv.reflect(&normalv);
+            let reflect_dot_eye = reflectv.dot(&eyev);
+            if reflect_dot_eye <= 0.0 {
+                black
+            } else {
+                let factor = reflect_dot_eye.powf(self.shininess());
+                light.intensity() * self.specular() * factor
+            }
+        };
+        ambient + diffuse + specular
     }
 }
 
@@ -82,5 +117,86 @@ mod materials_test {
     fn default_material_has_shininess() {
         let m = Material::default();
         assert_eq!(200.0, m.shininess());
-    }    
+    }
+
+    #[test]
+    fn lighting_with_eye_between_light_and_surface() {
+        let m = Material::default();
+        let position = Tup::point(0, 0, 0);
+        let eyev = Tup::vector(0, 0, -1);
+        let normalv = Tup::vector(0, 0, -1);
+        let light = Light::point_light(Tup::point(0, 0, -10), Color::new(1, 1, 1));
+        let result = m.lighting(light, position, eyev, normalv);
+        let sum_of_lights = m.ambient() + m.diffuse() + m.specular();
+        assert_eq!(
+            Color::new(sum_of_lights, sum_of_lights, sum_of_lights),
+            result
+        );
+    }
+
+    #[test]
+    fn lighting_with_eye_between_light_and_surface_eye_offset_45_degrees() {
+        let m = Material::default();
+        let position = Tup::point(0, 0, 0);
+        let eyev = Tup::vector(0.0, 2.0_f64.sqrt()/2.0, -2.0_f64.sqrt()/2.0);
+        let normalv = Tup::vector(0, 0, -1);
+        let light = Light::point_light(Tup::point(0, 0, -10), Color::new(1, 1, 1));
+        let result = m.lighting(light, position, eyev, normalv);
+        let sum_of_lights = m.ambient() + m.diffuse() + (0.0 * m.specular());
+        assert_eq!(
+            Color::new(sum_of_lights, sum_of_lights, sum_of_lights),
+            result
+        );
+    }
+
+    #[test]
+    fn lighting_with_eye_opposite_surface_light_offset_45_degrees() {
+        let m = Material::default();
+        let position = Tup::point(0, 0, 0);
+        let eyev = Tup::vector(0, 0, -1);
+        let normalv = Tup::vector(0, 0, -1);
+        let light = Light::point_light(Tup::point(0, 10, -10), Color::new(1, 1, 1));
+        let result = m.lighting(light, position, eyev, normalv);
+        let sum_of_lights = m.ambient()
+            + (2.0_f64.sqrt()/2.0 * m.diffuse())
+            + (0.0 * m.specular());
+        assert_eq!(
+            Color::new(sum_of_lights, sum_of_lights, sum_of_lights),
+            result
+        );
+    }
+
+    #[test]
+    fn lighting_with_eye_in_path_of_reflection() {
+        let m = Material::default();
+        let position = Tup::point(0, 0, 0);
+        let eyev = Tup::vector(0.0, -2.0_f64.sqrt()/2.0, -2.0_f64.sqrt()/2.0);
+        let normalv = Tup::vector(0, 0, -1);
+        let light = Light::point_light(Tup::point(0, 10, -10), Color::new(1, 1, 1));
+        let result = m.lighting(light, position, eyev, normalv);
+        let sum_of_lights = m.ambient()
+            + (2.0_f64.sqrt()/2.0 * m.diffuse())
+            + m.specular();
+        assert_eq!(
+            Color::new(sum_of_lights, sum_of_lights, sum_of_lights),
+            result
+        );
+    }
+
+    #[test]
+    fn lighting_with_light_behind_surface() {
+        let m = Material::default();
+        let position = Tup::point(0, 0, 0);
+        let eyev = Tup::vector(0, 0, 1);
+        let normalv = Tup::vector(0, 0, -1);
+        let light = Light::point_light(Tup::point(0, 0, 10), Color::new(1, 1, 1));
+        let result = m.lighting(light, position, eyev, normalv);
+        let sum_of_lights = m.ambient()
+            + (0.0 * m.diffuse())
+            + (0.0 * m.specular());
+        assert_eq!(
+            Color::new(sum_of_lights, sum_of_lights, sum_of_lights),
+            result
+        );
+    }
 }
