dff0cf5e726cdaee6c09bbb53dc2d2e08247c1b8
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-03-05 12:57:41 -0700

    Fix bug in BLUE color constant

--- src/color.rs
+++ src/color.rs
@@ -17,7 +17,7 @@ pub mod consts {
         Color { red: 0.0, green: 1.0, blue: 0.0 };
     
     pub const BLUE: Color =
-        Color { red: 0.0, green: 1.0, blue: 0.0 };
+        Color { red: 0.0, green: 0.0, blue: 1.0 };
 
     pub const MAGENTA: Color =
         Color { red: 1.0, green: 0.0, blue: 1.0 };
