0d7145783783276e6e39a625734b9edc8159ad76
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-03-12 02:50:10 -1000

    Add n1() and n2() to prepare calculations

--- src/intersections.rs
+++ src/intersections.rs
@@ -14,15 +14,18 @@ pub struct Computations {
     inside: bool,
     over_point: Tup,
     reflectv: Tup,
+    n1: f64,
+    n2: f64,
 }
 
 impl Computations {
-    fn new(intersection: &Intersection, ray: &Ray) -> Self {
+    fn new(intersection: &Intersection, ray: &Ray, xs: &Intersections) -> Self {
         let point = ray.position(intersection.t());
         let eyev = -ray.direction();
         let n = intersection.object().normal_at(point);
         let inside = n.dot(&eyev) < 0.0;
         let normalv = if inside { -n } else { n };
+        let (n1, n2) = Self::calc_n1_n2(&intersection, &xs);
         Self {
             intersection: intersection.clone(),
             point,
@@ -30,10 +33,49 @@ impl Computations {
             normalv,
             inside,
             over_point: point + (normalv * EPSILON),
-            reflectv: ray.direction().reflect(&normalv)
+            reflectv: ray.direction().reflect(&normalv),
+            n1,
+            n2,
         }
     }
-    
+
+    fn calc_n1_n2(intersection: &Intersection, xs: &Intersections) -> (f64, f64) {
+        let likely_eq = |o1: &Object, o2: &Object| {
+            println!("{:?}", o1);
+            format!("{:?}", o1) == format!("{:?}", o2)
+        };
+        let mut containers = Vec::new();
+        let mut n1 = 1.0;
+        let mut n2 = 1.0;
+        for i in 0..xs.len() {
+            let inter = xs[i].clone();
+            let is_hit = intersection.t() == inter.t();
+            if is_hit {
+                n1 = containers
+                    .last()
+                    .map_or(1.0, |j: &Object| j.material().refractive_index());
+            };
+
+            let index = containers.iter().position(|x| likely_eq(x, inter.object()));
+            match index {
+                Some(j) => {
+                    containers.remove(j);
+                }
+                None => {
+                    containers.push(inter.object().clone());
+                }
+            }
+
+            if is_hit {
+                n2 = containers
+                    .last()
+                    .map_or(1.0, |j: &Object| j.material().refractive_index());
+                break;
+            }
+        }
+        (n1, n2)
+    }
+
     pub fn t(&self) -> f64 {
         self.intersection.t()
     }
@@ -64,6 +106,14 @@ impl Computations {
 
     pub fn reflectv(&self) -> Tup {
         self.reflectv
+    }
+
+    pub fn n1(&self) -> f64 {
+        self.n1
+    }
+
+    pub fn n2(&self) -> f64 {
+        self.n2
     }
 }
 
@@ -102,11 +152,10 @@ impl Intersection {
     pub fn object(&self) -> &Object {
         &self.object
     }
-    
-    pub fn prepare_computations(&self, ray: Ray) -> Computations {
-        Computations::new(self, &ray)
-    }
 
+    pub fn prepare_computations(&self, ray: Ray, xs: &Intersections) -> Computations {
+        Computations::new(self, &ray, &xs)
+    }
 }
 
 pub struct Intersections {
@@ -122,10 +171,11 @@ impl Intersections {
 
     pub fn append(mut self, mut other: Intersections) -> Self {
         self.inters.append(&mut other.inters);
-        self.inters.sort_by(|a, b| a.t().partial_cmp(&b.t()).unwrap());
+        self.inters
+            .sort_by(|a, b| a.t().partial_cmp(&b.t()).unwrap());
         self
     }
-    
+
     pub fn push(mut self, i: Intersection) -> Self {
         self.inters.push(i);
         self
@@ -136,10 +186,13 @@ impl Intersections {
     }
 
     pub fn hit(&self) -> Option<&Intersection> {
-        self.inters.iter()
+        self.inters
+            .iter()
             .filter(|inter| inter.t() > 0.0)
             .min_by(|i1, i2| {
-                i1.t().partial_cmp(&i2.t()).expect("Intersections::hit got NaN")
+                i1.t()
+                    .partial_cmp(&i2.t())
+                    .expect("Intersections::hit got NaN")
             })
     }
 }
@@ -154,20 +207,19 @@ impl Index<usize> for Intersections {
 
 impl Default for Intersections {
     fn default() -> Self {
-        Self {
-            inters: Vec::new(),
-        }
+        Self { inters: Vec::new() }
     }
 }
 
 #[cfg(test)]
 mod intersections_test {
     use super::*;
-    use crate::test_helpers::assert_nearly_eq;
-    use crate::spheres::Sphere;
+    use crate::matrix::Mat4;
     use crate::planes::Plane;
-    use crate::transforms::translation;
-  
+    use crate::spheres::Sphere;
+    use crate::test_helpers::assert_nearly_eq;
+    use crate::transforms;
+
     #[test]
     fn an_intersection_encapsulates_a_t() {
         let s = Sphere::default();
@@ -183,7 +235,7 @@ mod intersections_test {
         assert_eq!(s.transform(), intersection.object().transform());
     }
 
-     #[test]
+    #[test]
     fn intersections_can_be_aggregated() {
         let s = Sphere::default();
         let i1 = Intersection::new(1.0, s);
@@ -213,7 +265,7 @@ mod intersections_test {
         let i2 = Intersection::new(1, s);
         let xs = Intersections::new(&vec![i1, i2]);
         let i = xs.hit();
-        assert_eq!(1.0, i.expect("No hit occured").t()); 
+        assert_eq!(1.0, i.expect("No hit occured").t());
     }
 
     #[test]
@@ -223,7 +275,7 @@ mod intersections_test {
         let i2 = Intersection::new(-1, s);
         let xs = Intersections::new(&vec![i1, i2]);
         let i = xs.hit();
-        assert!(i.is_none()); 
+        assert!(i.is_none());
     }
 
     #[test]
@@ -235,7 +287,7 @@ mod intersections_test {
         let i4 = Intersection::new(2, s);
         let xs = Intersections::new(&vec![i1, i2, i3, i4]);
         let i = xs.hit();
-        assert_eq!(2.0, i.expect("No hit occured").t()); 
+        assert_eq!(2.0, i.expect("No hit occured").t());
     }
 
     #[test]
@@ -243,7 +295,7 @@ mod intersections_test {
         let r = Ray::new(Tup::point(0, 0, -5), Tup::vector(0, 0, 1));
         let shape = Sphere::default();
         let i = Intersection::new(4, shape);
-        let comps = i.prepare_computations(r);
+        let comps = i.prepare_computations(r, &Intersections::new(&[i.clone()]));
         assert_eq!(comps.t(), i.t());
         assert_eq!(comps.object().material(), i.object().material());
         assert_eq!(comps.object().transform(), i.object().transform());
@@ -257,7 +309,7 @@ mod intersections_test {
         let r = Ray::new(Tup::point(0, 0, -5), Tup::vector(0, 0, 1));
         let shape = Sphere::default();
         let i = Intersection::new(4, shape);
-        let comps = i.prepare_computations(r);
+        let comps = i.prepare_computations(r, &Intersections::new(&[i.clone()]));
         assert!(!comps.inside());
     }
 
@@ -266,7 +318,7 @@ mod intersections_test {
         let r = Ray::new(Tup::point(0, 0, 0), Tup::vector(0, 0, 1));
         let shape = Sphere::default();
         let i = Intersection::new(1, shape);
-        let comps = i.prepare_computations(r);
+        let comps = i.prepare_computations(r, &Intersections::new(&[i.clone()]));
         assert_eq!(comps.point(), Tup::point(0, 0, 1));
         assert_eq!(comps.eyev(), Tup::vector(0, 0, -1));
         assert!(comps.inside());
@@ -276,19 +328,19 @@ mod intersections_test {
     #[test]
     fn the_hit_should_offset_the_point_by_a_small_amount() {
         let r = Ray::new(Tup::point(0, 0, -5), Tup::vector(0, 0, 1));
-        let shape = Sphere::default().with_transform(translation(0, 0, 1));
+        let shape = Sphere::default().with_transform(transforms::translation(0, 0, 1));
         let i = Intersection::new(5, shape);
-        let comps = i.prepare_computations(r);
-        assert!(comps.over_point().z < -EPSILON/2.0);
+        let comps = i.prepare_computations(r, &Intersections::new(&[i.clone()]));
+        assert!(comps.over_point().z < -EPSILON / 2.0);
     }
 
     #[test]
     fn the_hit_should_offset_in_the_direction_of_the_normal() {
         let r = Ray::new(Tup::point(0, 0, -5), Tup::vector(0, 0, 1));
-        let shape = Sphere::default().with_transform(translation(0, 0, 1));
+        let shape = Sphere::default().with_transform(transforms::translation(0, 0, 1));
         let i = Intersection::new(5, shape);
-        let comps = i.prepare_computations(r);
-        assert!(comps.point().z > comps.over_point().z);        
+        let comps = i.prepare_computations(r, &Intersections::new(&[i.clone()]));
+        assert!(comps.point().z > comps.over_point().z);
     }
 
     #[test]
@@ -296,9 +348,50 @@ mod intersections_test {
         let shape = Plane::default();
         let rad_2 = 2.0_f64.sqrt();
         let rad_2_over_2 = rad_2 / 2.0;
-        let r = Ray::new(Tup::point(0, 1, -1), Tup::vector(0.0, -rad_2_over_2, rad_2_over_2));
+        let r = Ray::new(
+            Tup::point(0, 1, -1),
+            Tup::vector(0.0, -rad_2_over_2, rad_2_over_2),
+        );
         let i = Intersection::new(rad_2, shape);
-        let comps = i.prepare_computations(r);
-        assert_eq!(comps.reflectv(), Tup::vector(0.0, rad_2_over_2, rad_2_over_2));        
+        let comps = i.prepare_computations(r, &Intersections::new(&[i.clone()]));
+        assert_eq!(
+            comps.reflectv(),
+            Tup::vector(0.0, rad_2_over_2, rad_2_over_2)
+        );
+    }
+
+    fn custom_glass_sphere(refractive_index: f64, transform: Mat4) -> Sphere {
+        let sphere = Sphere::glass_sphere().with_transform(transform);
+        let material = sphere.material().with_refractive_index(refractive_index);
+        sphere.with_material(material)
+    }
+
+    fn assert_n1_and_n2_of_at_intersection(i: usize, n1_expected: f64, n2_expected: f64) {
+        let outer_a = custom_glass_sphere(1.5, transforms::scaling(2, 2, 2));
+        let inner_b = custom_glass_sphere(2.0, transforms::translation(0.0, 0.0, -0.25));
+        let inner_c = custom_glass_sphere(2.5, transforms::translation(0.0, 0.0, 0.25));
+        let r = Ray::new(Tup::point(0, 0, 4), Tup::vector(0, 0, 1));
+        let xs = Intersections::new(&[
+            Intersection::new(2, outer_a),
+            Intersection::new(2.75, inner_b),
+            Intersection::new(3.25, inner_c),
+            Intersection::new(4.75, inner_b),
+            Intersection::new(5.25, inner_c),
+            Intersection::new(6, outer_a),
+        ]);
+        let comps = xs[i].prepare_computations(r, &xs);
+        println!("n1 = {}, n2 = {}", comps.n1(), comps.n2());
+        assert_nearly_eq(n1_expected, comps.n1());
+        assert_nearly_eq(n2_expected, comps.n2());
+    }
+
+    #[test]
+    fn correctly_calculates_n1_and_n2_at_different_iotersections() {
+        assert_n1_and_n2_of_at_intersection(0, 1.0, 1.5);
+        assert_n1_and_n2_of_at_intersection(1, 1.5, 2.0);
+        assert_n1_and_n2_of_at_intersection(2, 2.0, 2.5);
+        assert_n1_and_n2_of_at_intersection(3, 2.5, 2.5);
+        assert_n1_and_n2_of_at_intersection(4, 2.5, 1.5);
+        assert_n1_and_n2_of_at_intersection(5, 1.5, 1.0);
     }
 }
